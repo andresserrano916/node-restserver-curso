b821683da5156928fafde3a0be0e8a58bf58d8b4
Andres Serrano <andres.serrano916@gmail.com>
2019-02-02 19:38:12 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:qwert9876@ds113873.mlab.com:13873/cafedb';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
